52792fe53cd48b0d53e50d303ada4a9799487991
Swann Franceschi <toswann@gmail.com>
2014-04-28 13:03:57 -0400

    Chapter 7 done.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,8 @@
 /**
  * Created by swann on 27/04/2014.
  */
+Meteor.startup(function() {
+    Deps.autorun(function() {
+        console.log('There are ' + Posts.find().count() + ' posts');
+    });
+});
---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,21 @@
+/**
+ * Created by swann on 28/04/2014.
+ */
+Template.postSubmit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val(),
+            message: $(e.target).find('[name=message]').val()
+        }
+
+        Meteor.call('post', post, function(error, id) {
+            if (error)
+                return alert(error.reason);
+
+            Router.go('postPage', {_id: id});
+        });
+    }
+});
