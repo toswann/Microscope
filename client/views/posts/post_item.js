915656e80020c565505258db04ba05812c5c5da8
Swann Franceschi <toswann@gmail.com>
2014-04-28 15:55:56 -0400

    Chapter 8 done.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by swann on 27/04/2014.
  */
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId == Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
--- collections/posts.js
+++ collections/posts.js
@@ -3,12 +3,15 @@
  */
 Posts = new Meteor.Collection('posts');
 Posts.allow({
-    insert: function(userId, doc) {
-        // only allow posting if you are logged in
-        return !! userId;
+    update: ownsDocument,
+    remove: ownsDocument
+});
+Posts.deny({
+    update: function(userId, post, fieldNames) {
+        // may only edit the following two fields:
+        return (_.without(fieldNames, 'url', 'title').length > 0);
     }
 });
-
 Meteor.methods({
     post: function(postAttributes) {
         var user = Meteor.user(),
