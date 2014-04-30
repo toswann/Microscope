0d37c097ac1ae42021b5f13c4022a05d634f123e
Swann Franceschi <toswann@gmail.com>
2014-04-29 22:44:31 -0400

    Chapter 11 done.

---
+++ collections/notifications.js
@@ -0,0 +1,21 @@
+/**
+ * Created by swann on 29/04/2014.
+ */
+Notifications = new Meteor.Collection('notifications');
+
+Notifications.allow({
+    update: ownsDocument
+});
+
+createCommentNotification = function(comment) {
+    var post = Posts.findOne(comment.postId);
+    if (comment.userId !== post.userId) {
+        Notifications.insert({
+            userId: post.userId,
+            postId: post._id,
+            commentId: comment._id,
+            commenterName: comment.author,
+            read: false
+        });
+    }
+};
